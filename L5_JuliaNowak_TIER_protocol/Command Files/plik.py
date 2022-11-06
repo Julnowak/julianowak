40df5c859634ed31f8b7d2f1bb73e0fe5fb0aa31
Julnowak <91898403+Julnowak@users.noreply.github.com>
2022-11-06 18:04:29 +0100

    update

--- L5_JuliaNowak_TIER_protocol/Command Files/plik.py
+++ L5_JuliaNowak_TIER_protocol/Command Files/plik.py
@@ -3,7 +3,7 @@ import numpy as np
 
 ######################
 # Wstępne czyszczenie błędnych odczytów, odpowiedni podział wartości, szkielet tabeli i utworzenie DF
-with open("Original Data/weather.txt") as f:
+with open("../Original Data/weather.txt") as f:
     id = []
     year = []
     month = []
@@ -50,8 +50,6 @@ for i in range(31):
 
 df = pd.DataFrame(data)
 print(df)
-
-df.to_excel("../Command Files/Data_after_processing.xlsx", sheet_name='One_one')
 
 ###########################
 # Czyszczenie pustych miejsc, zgodnie z latami i ilością dni w miesiącu
@@ -102,3 +100,68 @@ for i in range(len(df1.index)):
 print(war)  # Do usunięcia
 df1.to_excel("../Command Files/New_clear.xlsx", sheet_name='One_one')
 
+
+df1 = pd.read_excel("../Command Files/Data_edited.xlsx")
+print(df1)
+
+########################
+# Tabela końcowa I
+
+tmax_final = []
+tmin_final = []
+date = []
+
+
+for i in range(len(df1.index)):
+    for d in range(31):
+        if df1.at[i, 'element'] == 'TMAX':
+            if df1.at[i, 'month'] in ['04', '06', '09', '11']:
+                if d >= 30:
+                    continue
+            elif df1.at[i, 'month'] == '02':
+                if int(df1.at[i, 'year']) % 4 == 0:
+                    if d >= 29:
+                        continue
+                else:
+                    if d >= 28:
+                        continue
+            tmax_final.append(df1.at[i, f'd{d+1}'])
+        elif df1.at[i, 'element'] == 'TMIN':
+            if df1.at[i, 'month'] in ['04', '06', '09', '11']:
+                if d >= 30:
+                    continue
+            elif df1.at[i, 'month'] == '02':
+                if int(df1.at[i, 'year']) % 4 == 0:
+                    if d >= 29:
+                        continue
+                else:
+                    if d >= 28:
+                        continue
+            tmin_final.append(df1.at[i, f'd{d+1}'])
+
+        if len(str(df1.at[i, 'month'])) == 1:
+            t = '0' + str(df1.at[i, 'month'])
+        else:
+            t = str(df1.at[i, 'month'])
+
+        if len(f'{d+1}') == 1:
+            b = '0' + f'{d+1}'
+        else:
+            b = f'{d+1}'
+
+        txt = str(df1.at[i, 'year']) + '-' + t + '-' + b
+        if txt not in date:
+            date.append(txt)
+
+
+aku = pd.read_excel("../Command Files/Data_edited.xlsx", usecols="A", skiprows=list(range(1, len(df1.index) + 1, 2)))
+data = dict()
+data['id'] = 'MX000017004'
+data['date'] = date
+data['tmax'] = tmax_final
+data['tmin'] = tmin_final
+
+df_final = pd.DataFrame(data)
+print(df_final)
+df_final = df_final.dropna()
+df_final.to_excel("../Analysis Data/Final_data.xlsx", sheet_name='One_one')
